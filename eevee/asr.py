e91ab872c628dc448b4887efbae4675419770a30
Abhinav Tushar <lepisma@fastmail.com>
2019-06-15 17:03:14 +0530

    Remove fluff

--- eevee/asr.py
+++ eevee/asr.py
@@ -4,45 +4,27 @@ WER evaluation functions
 
 from typing import List
 
-import requests
-from tqdm import tqdm
-from yamraz.tokenizer import tokenize
-
 from eevee.levenshtein import levenshtein
 
 
-def get_atlas_entity(plute_url: str, text: str):
-    try:
-        req = requests.post(plute_url, json={"text": text})
-        ents = [ent for ent in req.json()["response"]["aux_entities"] if ent["type"] == "location"]
-        return ents[0] if ents else None
-    except Exception:
-        return None
+def wer(ref_tokens: List[str], hyp_tokens: List[str]) -> int:
+    """
+    Raw Word Error Rate
+    """
 
-
-def wer(reference: str, hypothesis: str, lang: str) -> int:
-    ref_tokens = tokenize(reference, lang)
     if ref_tokens:
-        return levenshtein(ref_tokens, tokenize(hypothesis, lang)) / len(ref_tokens)
+        return levenshtein(ref_tokens, hyp_tokens) / len(ref_tokens)
     else:
         raise RuntimeError("Empty reference sentence")
 
 
-def atlas_error_rate(truth_texts: List[str], pred_texts: List[str], plute_url: str) -> float:
+def per(ref_ps: List[List[str]], hyp_ps: List[List[str]]) -> int:
     """
-    Return errors for all the cases where atlas should return something.
-
-    NOTE: Ideally this should be a precision/recall type thing
+    Phoneme Error Rate. Inputs are lists of pronunciations (represented as list
+    of phonemes). Error which is minimum across the cross join between
+    hypothesis and reference is reported. Note that it's easy to fool this by
+    passing an exhaustive list of pronunciations in hypothesis and get low PER.
+    As of yet, we don't worry about such situations.
     """
 
-    cases = 0
-    hits = 0
-    for truth, pred in tqdm(zip(truth_texts, pred_texts), total=len(truth_texts)):
-        ent = get_atlas_entity(plute_url, truth)
-        if ent:
-            cases += 1
-            pred_ent = get_atlas_entity(plute_url, pred)
-            if pred_ent and pred_ent["values"][0]["value"] == ent["values"][0]["value"]:
-                hits += 1
-
-    return 1 - (hits / cases)
+    raise NotImplementedError()
--- tests/test_asr.py
+++ tests/test_asr.py
@@ -1,5 +1,4 @@
 import pytest
-from yamraz.tokenizer import tokenize
 
 from eevee.levenshtein import levenshtein
 
@@ -7,13 +6,12 @@ from eevee.levenshtein import levenshtein
 # NOTE: This test keeps insertion, deletion and substitution information also
 #       but for now we are only using the the total cost
 @pytest.mark.parametrize("reference, hypothesis, output", [
-    ("hello world", "hello world", (0, 0, 0, 0)),
-    ("hello world", "hello", (1, 0, 1, 0)),
-    ("hello world", "errr", (2, 0, 1, 1)),
-    ("hello world", "hello that world", (1, 1, 0, 0)),
-    ("hello world", "hola world", (1, 0, 0, 1)),
-    ("hello world", "", (2, 0, 2, 0))
+    ("hello world".split(), "hello world".split(), (0, 0, 0, 0)),
+    ("hello world".split(), "hello".split(), (1, 0, 1, 0)),
+    ("hello world".split(), "errr".split(), (2, 0, 1, 1)),
+    ("hello world".split(), "hello that world".split(), (1, 1, 0, 0)),
+    ("hello world".split(), "hola world".split(), (1, 0, 0, 1)),
+    ("hello world".split(), "".split(), (2, 0, 2, 0))
 ])
 def test_wer(reference, hypothesis, output):
-    lang = "en"
-    assert levenshtein(tokenize(reference, lang), tokenize(hypothesis, lang)) == output[0]
+    assert levenshtein(reference, hypothesis) == output[0]
