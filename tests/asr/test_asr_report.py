19b6555d929f655c06e178cc04831efc3afb5e5a
Sachin Kumar <sachinm1596@gmail.com>
2022-02-16 17:08:54 +0530

    update asr tests

--- tests/asr/test_asr_report.py
+++ tests/asr/test_asr_report.py
@@ -1,18 +1,39 @@
-
 import pandas as pd
 from eevee.metrics.asr import asr_report
 
 
 def test_asr_report():
 
-    true_df = pd.read_csv("data/tagged.transcriptions.csv", usecols=["id", "transcription"])
-    pred_df = pd.read_csv("data/predicted.transcriptions.csv", usecols=["id", "utterances"])
+    true_df = pd.read_csv(
+        "data/tagged.transcriptions.csv", usecols=["id", "transcription"]
+    )
+    pred_df = pd.read_csv(
+        "data/predicted.transcriptions.csv", usecols=["id", "utterances"]
+    )
 
     expected = pd.DataFrame(
         {
-            "Metric": ["WER", "Utterance FPR", "Utterance FNR", "SER"],
-            "Value": [0.5714285714285715, 0.5, 0.25, 0.6666666666666666],
-            "Support": [6, 2, 4, 6],
+            "Metric": [
+                "WER",
+                "Utterance FPR",
+                "Utterance FNR",
+                "SER",
+                "Min 3 WER",
+                "Min WER",
+                "Short Utterance WER",
+                "Long Utterance WER",
+            ],
+            "Value": [
+                0.5714285714285715,
+                0.5,
+                0.25,
+                0.6666666666666666,
+                0.5714285714285715,
+                0.5714285714285715,
+                0.000000,
+                0.8095238095238096,
+            ],
+            "Support": [6, 2, 4, 6, 6, 6, 1, 3],
         }
     )
     expected.set_index("Metric", inplace=True)
