4d05f2bbd5eb954aa87b0c5da87af6b3f79c21b7
Manas <kmanas@vernacular.ai>
2022-05-24 18:51:41 +0530

    updated tests for asr noisy

--- tests/asr/test_process_noise_info.py
+++ tests/asr/test_process_noise_info.py
@@ -4,12 +4,13 @@ from eevee.metrics.asr import extract_info_tags, remove_info_tags, define_noisy
 def test_process_noise_info():
 
 	clean_transcription = "This is an example"
-	noise_tags = ["", "['<audio_silent>']", "['<inauidble>']", "['<background_speech>']"]
+	noise_tags = ["", "<audio_silent>", "<inauidble>", "<background_speech>"]
 	noise_labels = [0, 0, 1, 1]
 
 	noisy_transcriptions = ["{}{}".format(clean_transcription, tag) for tag in noise_tags]
 
 	for transcription, tag, label in zip(noisy_transcriptions, noise_tags, noise_labels):
-		assert extract_info_tags(transcription) == tag
+		tags = [tag] if tag != "" else []
+		assert extract_info_tags(transcription) == tags
 		assert remove_info_tags(transcription) == clean_transcription
-		assert define_noisy(tag) == label
+		assert define_noisy(tags) == label
