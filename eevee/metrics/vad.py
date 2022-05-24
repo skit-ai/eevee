7215ef7e98eadda7cf263fa87c0f020a39f0f3e7
Manas <kmanas@vernacular.ai>
2022-05-25 01:17:19 +0530

    black formatting

--- eevee/metrics/vad.py
+++ eevee/metrics/vad.py
@@ -10,21 +10,29 @@ def return_params(params: Dict):
     return params["error"], params["cutoff"]
 
 
-def map_types(label):
-    if label!="SPEECH":
+def map_types(label) -> str:
+    if label != "SPEECH":
         return "NON-SPEECH"
     return label
 
 
-def is_captured(pred_seg: Dict, true_segments: List[Dict], error: float = .1, cutoff: float = .2):
+def is_captured(
+    pred_seg: Dict, true_segments: List[Dict], error: float = 0.1, cutoff: float = 0.2
+) -> int:
     pred_start = pred_seg["time-range"][0]
     for seg in true_segments:
-        if ((pred_start<seg["time-range"][1]) and (0 < pred_start - seg["time-range"][0] < error+cutoff) or (0 < seg["time-range"][0] - pred_start < error)):
+        if (
+            (pred_start < seg["time-range"][1])
+            and (0 < pred_start - seg["time-range"][0] < error + cutoff)
+            or (0 < seg["time-range"][0] - pred_start < error)
+        ):
             return 1
     return 0
 
 
-def barge_in_report(true_labels: pd.DataFrame, pred_labels: pd.DataFrame, params: Dict) -> pd.DataFrame:
+def barge_in_report(
+    true_labels: pd.DataFrame, pred_labels: pd.DataFrame, params: Dict
+) -> Dict:
 
     # read params
     ID, TRUTH, PREDICTED = return_columns(params["data-columns"])
@@ -35,16 +43,32 @@ def barge_in_report(true_labels: pd.DataFrame, pred_labels: pd.DataFrame, params
     data[PREDICTED].fillna("[]", inplace=True)
 
     # collect SPEECH segments
-    data["truth-speech"] = data[TRUTH].apply(lambda tag: [seg for seg in tag if map_types(seg["type"]) == "SPEECH" and seg["time-range"][1] - seg["time-range"][0] > ERROR])
-    data["predicted-speech"] = data[PREDICTED].apply(lambda tag: [seg for seg in tag if map_types(seg["type"]) == "SPEECH"])
+    data["truth-speech"] = data[TRUTH].apply(
+        lambda tag: [
+            seg
+            for seg in tag
+            if map_types(seg["type"]) == "SPEECH"
+            and seg["time-range"][1] - seg["time-range"][0] > ERROR
+        ]
+    )
+    data["predicted-speech"] = data[PREDICTED].apply(
+        lambda tag: [seg for seg in tag if map_types(seg["type"]) == "SPEECH"]
+    )
 
     # calculate metrics
     data["speech-captured"] = data.apply(
-        lambda row: [is_captured(pred_seg, row["truth-speech"], error=ERROR, cutoff=CUTOFF) for pred_seg in
-                     row["predicted-speech"]], axis=1)
-    captures = data["speech-captured"].apply(lambda captures: len([x for x in captures if x == 1])).sum()
+        lambda row: [
+            is_captured(pred_seg, row["truth-speech"], error=ERROR, cutoff=CUTOFF)
+            for pred_seg in row["predicted-speech"]
+        ],
+        axis=1,
+    )
+    captures = (
+        data["speech-captured"]
+        .apply(lambda captures: len([x for x in captures if x == 1]))
+        .sum()
+    )
     precision = captures / data["predicted-speech"].sum()
     recall = captures / data["truth-speech"].sum()
 
     return {"precision": precision, "recall": recall}
-
--- scripts/eval_barge_in.py
+++ scripts/eval_barge_in.py
@@ -23,10 +23,10 @@ from eevee.utils import parse_yaml
 
 if __name__ == "__main__":
 
-	args = docopt(__doc__)
-	true_labels = pd.read_csv(args["<true-labels>"])
-	pred_labels = pd.read_csv(args["<pred-labels>"])
-	vad_params = parse_yaml(args["--vad-yaml"])
+    args = docopt(__doc__)
+    true_labels = pd.read_csv(args["<true-labels>"])
+    pred_labels = pd.read_csv(args["<pred-labels>"])
+    vad_params = parse_yaml(args["--vad-yaml"])
 
-	output = barge_in_report(true_labels, pred_labels, vad_params)
-	print(output)
+    output = barge_in_report(true_labels, pred_labels, vad_params)
+    print(output)
