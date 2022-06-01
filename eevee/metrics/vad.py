99ba54e373fdfb7f274a044c9b271d36600992e8
Manas <kmanas@vernacular.ai>
2022-06-01 15:56:45 +0530

    updated barge-in eval

--- eevee/metrics/vad.py
+++ eevee/metrics/vad.py
@@ -17,16 +17,20 @@ def map_types(label) -> str:
     return label
 
 
-def is_captured(
-    pred_seg: Dict, true_segments: List[Dict], error: float = 0.1, cutoff: float = 0.2
+def is_captured(true_seg, pred_seg, error: float = 0.1, cutoff: float = 0.2):
+    true_start = true_seg["time-range"][0]
+    return (
+        (true_start < pred_seg["time-range"][1])
+        and (0 < true_start - pred_seg["time-range"][0] < error + cutoff)
+        or (0 < pred_seg["time-range"][0] - true_start < error)
+    )
+
+
+def match_predictions(
+    true_seg: Dict, pred_segments: List[Dict], error: float = 0.1, cutoff: float = 0.2
 ) -> int:
-    pred_start = pred_seg["time-range"][0]
-    for seg in true_segments:
-        if (
-            (pred_start < seg["time-range"][1])
-            and (0 < pred_start - seg["time-range"][0] < error + cutoff)
-            or (0 < seg["time-range"][0] - pred_start < error)
-        ):
+    for pred_seg in pred_segments:
+        if is_captured(true_seg, pred_seg, error=error, cutoff=cutoff):
             return 1
     return 0
 
@@ -61,7 +65,7 @@ def barge_in_report(
     # calculate metrics
     data["speech-captured"] = data.apply(
         lambda row: [
-            is_captured(pred_seg, row["truth-speech"], error=ERROR, cutoff=CUTOFF)
+            match_predictions(pred_seg, row["truth-speech"], error=ERROR, cutoff=CUTOFF)
             for pred_seg in row["predicted-speech"]
         ],
         axis=1,
