eafec6e1c142a1d2e39a269f145f1b3a38814646
Manas <kmanas@vernacular.ai>
2022-08-03 19:32:39 +0530

    updated metric calculations to be on a per-turn basis

--- eevee/metrics/vad.py
+++ eevee/metrics/vad.py
@@ -11,25 +11,39 @@ def return_params(params: Dict):
     return params["error"], params["cutoff"]
 
 
-def map_types(label) -> str:
-    if label != "SPEECH":
+def map_type(segment: Dict) -> str:
+    if segment["type"] != "SPEECH":
         return "NON-SPEECH"
-    return label
+    return segment["type"]
 
 
-def is_captured(true_seg, pred_seg, error: float = 0.1, cutoff: float = 0.2):
-    true_start = true_seg["time-range"][0]
+def map_length(segment: Dict) -> float:
+    return segment["time-range"][1] - segment["time-range"][0]
+
+
+def is_captured(true_segment, pred_segment, error: float = 0.1, cutoff: float = 0.2):
+
+    true_start, true_end = true_segment["time-range"]
+    pred_start, pred_end = pred_segment["time-range"]
+
     return (
-        (true_start < pred_seg["time-range"][1])
-        and (0 < true_start - pred_seg["time-range"][0] < error + cutoff)
-        or (0 < pred_seg["time-range"][0] - true_start < error)
+        (true_start < pred_start)
+        and (pred_start - (true_start-error) < cutoff)
     )
 
 
-def match_predictions(
+def match_truth(
     true_seg: Dict, pred_segments: List[Dict], error: float = 0.1, cutoff: float = 0.2
 ) -> int:
     for pred_seg in pred_segments:
+        if is_captured(true_seg, pred_seg, error=error, cutoff=cutoff):
+            return 1
+    return 0
+
+def match_prediction(
+    pred_seg: Dict, true_segments: List[Dict], error: float = 0.1, cutoff: float = 0.2
+) -> int:
+    for true_seg in true_segments:
         if is_captured(true_seg, pred_seg, error=error, cutoff=cutoff):
             return 1
     return 0
@@ -43,56 +57,60 @@ def barge_in_report(
     ID, TRUTH, PREDICTED = return_columns(params["data-columns"])
     ERROR, CUTOFF = return_params(params["barge-in"])
 
-    # merge truth and predictions
+    # merge truth and predictions. fill in empty predictions
     data = pd.merge(true_labels, pred_labels, on=ID, how="outer")
     data[PREDICTED].fillna("[]", inplace=True)
 
-    # collect SPEECH segments
+    # collect SPEECH segments, and keep a count
     data["truth-speech"] = data[TRUTH].apply(
-        lambda tag: [
-            seg
-            for seg in ast.literal_eval(tag)
-            if map_types(seg["type"]) == "SPEECH"
-            and seg["time-range"][1] - seg["time-range"][0] > ERROR
-        ]
+        lambda tag: sorted(
+            [
+                seg
+                for seg in ast.literal_eval(tag)
+                if (map_type(seg) == "SPEECH")
+                # and (map_length(seg) > ERROR)
+            ],
+            key=lambda seg: seg["time-range"][0]
+        )
     )
+    data["truth-speech-exists"] = data["truth-speech"].apply(
+        lambda segments: 1 if len(segments) > 0 else 0
+    )
+
     data["predicted-speech"] = data[PREDICTED].apply(
-        lambda tag: [
-            seg for seg in ast.literal_eval(tag) if map_types(seg["type"]) == "SPEECH"
-        ]
+        lambda tag: sorted(
+            [
+                seg
+                for seg in ast.literal_eval(tag)
+                if (map_type(seg) == "SPEECH")
+            ],
+            key=lambda seg: seg["time-range"][0]
+        )
+    )
+    data["predicted-speech-exists"] = data["predicted-speech"].apply(
+        lambda segments: 1 if len(segments) > 0 else 0
     )
 
     # calculate metrics
     data["truth-captured"] = data.apply(
         lambda row: [
-            match_predictions(seg, row["predicted-speech"], error=ERROR, cutoff=CUTOFF)
+            match_truth(seg, row["predicted-speech"], error=ERROR, cutoff=CUTOFF)
             for seg in row["truth-speech"]
         ],
         axis=1,
     )
-    truth_captures = (
-        data["truth-captured"]
-        .apply(lambda captures: len([x for x in captures if x == 1]))
-        .sum()
-    )
+    truth_captures = data["truth-captured"].apply(lambda captures: 1 if 1 in captures else 0).sum()
 
     data["predicted-captured"] = data.apply(
         lambda row: [
-            match_predictions(seg, row["truth-speech"], error=ERROR, cutoff=CUTOFF)
+            match_prediction(seg, row["truth-speech"], error=ERROR, cutoff=CUTOFF)
             for seg in row["predicted-speech"]
         ],
         axis=1,
     )
-    predicted_captures = (
-        data["predicted-captured"]
-        .apply(lambda captures: len([x for x in captures if x == 1]))
-        .sum()
-    )
+    predicted_captures = data["predicted-captured"].apply(lambda captures: 1 if captures[0] == 1 else 0).sum()
 
-    precision = (
-        predicted_captures
-        / data["predicted-speech"].apply(lambda segs: len(segs)).sum()
-    )
-    recall = truth_captures / data["truth-speech"].apply(lambda segs: len(segs)).sum()
+    recall = truth_captures / data["truth-speech-exists"].sum()
+    precision = predicted_captures / data["predicted-speech-exists"].sum()
 
     return {"precision": precision, "recall": recall}
